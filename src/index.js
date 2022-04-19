9479d00731c612c55915407c6e008ba8c6ff825d
Guy Kibby <GuyKibby@gmail.com>
2022-04-20 10:40:08 +1200

    added grid

--- src/index.js
+++ src/index.js
@@ -1,17 +1,55 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React, { useState } from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+
+let rows = 30;
+let cols = 50;
+// let speed = 100;
+
+let gridFull = Array(rows)
+  .fill()
+  .map(() => Array(cols).fill(false));
+
+const Box = ({ boxClass, boxId, row, col }) => {
+  return <div className={boxClass}></div>;
+};
+
+const Grid = () => {
+  const width = cols * 16;
+  let rowsArr = [];
+
+  for (let i = 0; i < rows; i++) {
+    for (let j = 0; j < cols; j++) {
+      let boxId = i + " " + j;
+      let boxClass = gridFull[i][j] ? "box on" : "box off";
+      rowsArr.push(
+        <Box boxClass={boxClass} boxId={boxId} key={boxId} row={i} col={j} />
+      );
+    }
+  }
+
+  return (
+    <div className="grid" style={{ width: width }}>
+      {rowsArr}
+    </div>
+  );
+};
+
+const Main = () => {
+  const [genNum, setGen] = useState(0);
+
+  return (
+    <>
+      <h1>The Game of Life</h1>
+      <Grid />
+      <h2>Generations: {genNum}</h2>
+    </>
+  );
+};
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Main />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
