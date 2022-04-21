c6be00e7a0901eff6a9c5fabf54d65263d10fdcf
Guy Kibby <GuyKibby@gmail.com>
2022-04-21 20:36:42 +1200

    CSS added

--- src/index.js
+++ src/index.js
@@ -5,8 +5,8 @@ import Grid from "./Grid.js";
 
 let rows = 30;
 let cols = 50;
-let speed = 100;
 let genNum = 0;
+let speed = 100;
 let grid = Array(rows)
   .fill()
   .map(() => Array(cols).fill(false));
@@ -15,120 +15,186 @@ const arrayClone = (array) => {
   return JSON.parse(JSON.stringify(array));
 };
 
+const switchBoxState = (row, col) => {
+  let gridCopy = arrayClone(grid);
+  gridCopy[row][col] = !gridCopy[row][col];
+  grid = gridCopy;
+};
+
+const seedGrid = () => {
+  let gridCopy = arrayClone(grid);
+
+  gridCopy = gridCopy.map((rowArray) =>
+    rowArray.map((cell) => {
+      if (Math.floor(Math.random() * 4) === 1) {
+        return true;
+      } else {
+        return false;
+      }
+    })
+  );
+
+  genNum = 0;
+  grid = gridCopy;
+};
+
+seedGrid();
+
+const clearGrid = () => {
+  grid = Array(rows)
+    .fill()
+    .map(() => Array(cols).fill(false));
+};
+
+const nextGen = () => {
+  let gridCopy = arrayClone(grid);
+
+  for (let i = 0; i < rows; i++) {
+    for (let j = 0; j < cols; j++) {
+      let neighbours = 0;
+      if (i > 0 && j > 0) {
+        if (grid[i - 1][j - 1]) {
+          neighbours++;
+        }
+      }
+      if (i > 0) {
+        if (grid[i - 1][j]) {
+          neighbours++;
+        }
+      }
+      if (i > 0 && j < cols - 1) {
+        if (grid[i - 1][j + 1]) {
+          neighbours++;
+        }
+      }
+      if (j < cols - 1) {
+        if (grid[i][j + 1]) {
+          neighbours++;
+        }
+      }
+      if (i < rows - 1 && j < cols - 1) {
+        if (grid[i + 1][j + 1]) {
+          neighbours++;
+        }
+      }
+      if (i < rows - 1) {
+        if (grid[i + 1][j]) {
+          neighbours++;
+        }
+      }
+      if (i < rows - 1 && j > 0) {
+        if (grid[i + 1][j - 1]) {
+          neighbours++;
+        }
+      }
+      if (j > 0) {
+        if (grid[i][j - 1]) {
+          neighbours++;
+        }
+      }
+
+      if (gridCopy[i][j] === true) {
+        if (neighbours < 2 || neighbours > 3) {
+          gridCopy[i][j] = false;
+        }
+      } else if (neighbours === 3) {
+        gridCopy[i][j] = true;
+      }
+    }
+  }
+
+  grid = gridCopy;
+  genNum += 1;
+};
+
 const Main = () => {
-  // const [genNum, setGen] = useState(0);
-  // const [theState, setState] = useState("STOP");
   const [intervalId, setIntervalId] = useState(0);
   const [gridFull, setGrid] = useState(grid);
 
-  const switchBoxState = (row, col) => {
-    let gridCopy = [...gridFull];
-    gridCopy[row][col] = !gridCopy[row][col];
-    setGrid(gridCopy);
-  };
-
-  useEffect(() => seedGrid(), []);
-
-  const seedGrid = () => {
-    let gridCopy = [...gridFull];
-
-    gridCopy = gridCopy.map((rowArray) =>
-      rowArray.map((cell) => {
-        if (Math.floor(Math.random() * 4) === 1) {
-          return true;
-        } else {
-          return false;
-        }
-      })
-    );
-    // setGen(0);
-    genNum = 0;
-    setGrid(gridCopy);
-  };
-
-  const nextGen = () => {
-    let gridCopy = [...gridFull];
-    // let gridCopy = arrayClone(gridFull);
-    // let gridCopy = gridFull.map((arr) => arr.slice());
-    // console.log(gridCopy[-1][-1]);
-    for (let i = 0; i < rows; i++) {
-      for (let j = 0; j < cols; j++) {
-        let neighbours = 0;
-        if (i > 0 && j > 0) {
-          if (gridFull[i - 1][j - 1]) {
-            neighbours++;
-          }
-        }
-        if (i > 0) {
-          if (gridFull[i - 1][j]) {
-            neighbours++;
-          }
-        }
-        if (i > 0 && j < cols - 1) {
-          if (gridFull[i - 1][j + 1]) {
-            neighbours++;
-          }
-        }
-        if (j < cols - 1) {
-          if (gridFull[i][j + 1]) {
-            neighbours++;
-          }
-        }
-        if (i < rows - 1 && j < cols - 1) {
-          if (gridFull[i + 1][j + 1]) {
-            neighbours++;
-          }
-        }
-        if (i < rows - 1) {
-          if (gridFull[i + 1][j]) {
-            neighbours++;
-          }
-        }
-        if (i < rows - 1 && j > 0) {
-          if (gridFull[i + 1][j - 1]) {
-            neighbours++;
-          }
-        }
-        if (j > 0) {
-          if (gridFull[i][j - 1]) {
-            neighbours++;
-          }
-        }
-
-        if (gridCopy[i][j] === true) {
-          if (neighbours < 2 || neighbours > 3) {
-            gridCopy[i][j] = false;
-          }
-        } else if (neighbours === 3) {
-          gridCopy[i][j] = true;
-        }
-      }
-    }
-
-    setGrid(gridCopy);
-    genNum += 1;
-  };
-
   const start = () => {
-    const newIntervalId = setInterval(nextGen, speed);
+    const newIntervalId = setInterval(() => {
+      nextGen();
+      setGrid(grid);
+    }, speed);
     setIntervalId(newIntervalId);
   };
 
-  const stop = () => {
+  const pause = () => {
     clearInterval(intervalId);
     setIntervalId(0);
+  };
+
+  const switchBox = (row, col) => {
+    switchBoxState(row, col);
+    setGrid(grid);
+  };
+
+  const seed = () => {
+    seedGrid();
+    setGrid(grid);
+  };
+
+  const clear = () => {
+    clearGrid();
+    setGrid(grid);
+  };
+
+  const spawn = () => {
+    nextGen();
+    setGrid(grid);
+  };
+
+  const slow = () => {
+    if (speed < 2000) {
+      speed = speed * 2;
+    }
+
+    if (intervalId !== 0) {
+      pause();
+      start();
+    }
+    console.log(speed);
+  };
+
+  const fast = () => {
+    if (speed > 3) {
+      speed = speed / 2;
+    }
+
+    if (intervalId !== 0) {
+      pause();
+      start();
+    }
+    console.log(speed);
   };
 
   return (
     <>
       <h1>The Game of Life</h1>
-      <div>
-        <button onClick={start}>START</button>
-        <button onClick={stop}>STOP</button>
-        <button onClick={seedGrid}>SEED</button>
-        <button onClick={nextGen}>nextGen</button>
+      <div className="buttonBar">
+        <button onClick={start} disabled={intervalId !== 0}>
+          START
+        </button>
+        <button onClick={pause} disabled={intervalId === 0}>
+          PAUSE
+        </button>
+        <button onClick={clear} disabled={intervalId !== 0}>
+          CLEAR
+        </button>
+        <button onClick={seed} disabled={intervalId !== 0}>
+          SEED
+        </button>
+        <button onClick={spawn} disabled={intervalId !== 0}>
+          SPAWN
+        </button>
+        <button onClick={slow} disabled={intervalId === 0}>
+          SLOWER
+        </button>
+        <button onClick={fast} disabled={intervalId === 0}>
+          FASTER
+        </button>
       </div>
-      <Grid theGrid={gridFull} callback={switchBoxState} />
+      <Grid theGrid={gridFull} callback={switchBox} />
       <h2>Generations: {genNum}</h2>
     </>
   );
