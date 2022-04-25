554deaf9cc27fd4b6d04f5c6c209a00b8f126979
Guy Kibby <GuyKibby@gmail.com>
2022-04-25 13:32:48 +1200

    done

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import Grid from "./Grid.js";
